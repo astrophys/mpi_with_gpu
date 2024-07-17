654988267cae97585496df86d9457dc395898caa
Ali <ali@markiiisys.com>
2024-07-17 07:44:31 -0500

    Reorganized repo so I can generate two executables. 1. Modified Makefile and it works with the new location of *.cu, *.h, and *.o files. 2. Added src/mpi/nccl_matrix_mult.cu as my first attempt to use the NCCL libs

--- Makefile
+++ Makefile
@@ -4,20 +4,22 @@
 #
 
 # changed 7/9/2024
-LIB = -L/cm/local/apps/cuda/libs/current/lib64 -L/cm/shared/apps/openmpi4/gcc/4.1.5/lib -lmpi
-
-
+###### mpi_matrix_mult ######
+MPI_LIB = -L/cm/local/apps/cuda/libs/current/lib64 -L/cm/shared/apps/openmpi4/gcc/4.1.5/lib -lmpi
 CXXFLAGS = -Wall -g
-PREFIX = src
+PREFIX_MPI = src/mpi
 CC = mpic++
 NVCC = nvcc
 NVCCFLAGS = --compiler-options ${CXXFLAGS} -I${CPATH} -I${CUDA_INC_PATH}/include
 
-OBJ = $(OBJ_DIR)/mpi_matrix_mult.o $(OBJ_DIR)/functions.o $(OBJ_DIR)/cpu_mult.o $(OBJ_DIR)/gpu_mult.o
-OBJ_DIR = $(PREFIX)/obj
+OBJ_DIR_MPI = $(PREFIX_MPI)/obj
+OBJ_MPI = $(OBJ_DIR_MPI)/mpi_matrix_mult.o $(OBJ_DIR_MPI)/functions.o $(OBJ_DIR_MPI)/cpu_mult.o $(OBJ_DIR_MPI)/gpu_mult.o
 #CU_OBJ = $(OBJ_DIR)/functions.o
 
 # E.g. of working compilation nvcc -I/cm/shared/apps/openmpi4/gcc/4.1.2/include -I${CUDA_INC_PATH}/include -L/cm/local/apps/cuda/libs/current/lib64 -L/cm/shared/apps/openmpi4/gcc/4.1.2/lib -lmpi -o mpi_matrix_mult src/obj/mpi_matrix_mult.o
+
+
+###### nccl_matrix_mult ######
 
 all: mpi_matrix_mult
 
@@ -27,11 +29,14 @@ all: mpi_matrix_mult
 
 # Compile straight CUDA files
 # -dc avoids error : ptxas fatal   : Unresolved extern function '_Z9d_map_idxiii'
-$(OBJ_DIR)/%.o : $(PREFIX)/%.cu
+$(OBJ_DIR_MPI)/%.o : $(PREFIX_MPI)/%.cu
 	$(NVCC) $(NVCCFLAGS) $(CPLUS_INCLUDE_PATH) -dc $< -o $@
 
-mpi_matrix_mult : $(OBJ)
-	$(NVCC) $(NVCCFLAGS) $(CPLUS_INCLUDE_PATH) -o mpi_matrix_mult $(LIB) $(OBJ)
+mpi_matrix_mult : $(OBJ_MPI)
+	$(NVCC) $(NVCCFLAGS) $(CPLUS_INCLUDE_PATH) -o mpi_matrix_mult $(MPI_LIB) $(OBJ_MPI)
+
+#nccl_matrix_mult : $(OBJ)
+#	$(NVCC) $(NVCCFLAGS) $(CPLUS_INCLUDE_PATH) -o mpi_matrix_mult $(LIB) $(OBJ)
 
 clean :
-	rm $(OBJ) mpi_matrix_mult
+	rm $(OBJ_MPI) mpi_matrix_mult
---
+++ src/mpi/nccl_matrix_mult.cu
@@ -0,0 +1,124 @@
+/**************************************************************
+ *  AUTHOR : Ali Snedden
+ *  DATE   : 07/15/24
+ *  LICENSE: GPL-3
+ *  PURPOSE: To use my MPI code and adapting it to use NVidia's NCCL library
+ *           This code does numerical integration using the trapezoidal rule.
+ *  NOTES  :
+ *      1. To compile and run : 
+ *          $ ml load MVAPICH2/2.3.4
+ *          $ mpicc mpi_integrate.c
+ *          $ srun --ntasks=2 --nodes=2 --pty bash
+ *          $ mpirun -np 2 ./a.out
+ *
+ *      2. To debug :
+ *          $ ml load MVAPICH2/2.3.4
+ *          $ mpicc -ggdb mpi_integrate.c
+ *          $ mpirun -np 2 xterm -e gdb ./a.out
+ *
+ *  REFERNCES :
+ *      1. NCCL Example : https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/examples.html
+ *      2.
+ *      3. 
+ *      4.
+ *      5. 
+ *
+ *
+ *
+ *************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime_api.h>
+#include <nccl.h>
+//#include <unistd.h>
+//#include <math.h>
+//#include <time.h>
+//#include "functions.h"
+//#include "cpu_mult.h"
+//#include "gpu_mult.h"
+//#include <iostream>
+using namespace std;
+
+#define CUDACHECK(cmd) do {                         \
+  cudaError_t err = cmd;                            \
+  if (err != cudaSuccess) {                         \
+    printf("Failed: Cuda error %s:%d '%s'\n",       \
+        __FILE__,__LINE__,cudaGetErrorString(err)); \
+    exit(EXIT_FAILURE);                             \
+  }                                                 \
+} while(0)
+
+
+#define NCCLCHECK(cmd) do {                         \
+  ncclResult_t res = cmd;                           \
+  if (res != ncclSuccess) {                         \
+    printf("Failed, NCCL error %s:%d '%s'\n",       \
+        __FILE__,__LINE__,ncclGetErrorString(res)); \
+    exit(EXIT_FAILURE);                             \
+  }                                                 \
+} while(0)
+
+int main(int argc, char * argv[])
+{
+    /***********************************************************/
+    /****************** Variable Declaration *******************/
+    /***********************************************************/
+    ncclComm_t comms[4];
+    //managing 4 devices
+    int nDev = 4;
+    int size = 32*1024*1024;
+    int devs[4] = { 0, 1, 2, 3 };
+
+    //allocating and initializing device buffers
+    float** sendbuff = (float**)malloc(nDev * sizeof(float*));
+    float** recvbuff = (float**)malloc(nDev * sizeof(float*));
+    cudaStream_t* s = (cudaStream_t*)malloc(sizeof(cudaStream_t)*nDev);
+
+
+    for (int i = 0; i < nDev; ++i) {
+      CUDACHECK(cudaSetDevice(i));
+      CUDACHECK(cudaMalloc((void**)sendbuff + i, size * sizeof(float)));
+      CUDACHECK(cudaMalloc((void**)recvbuff + i, size * sizeof(float)));
+      CUDACHECK(cudaMemset(sendbuff[i], 1, size * sizeof(float)));
+      CUDACHECK(cudaMemset(recvbuff[i], 0, size * sizeof(float)));
+      CUDACHECK(cudaStreamCreate(s+i));
+    }
+
+
+    //initializing NCCL
+    NCCLCHECK(ncclCommInitAll(comms, nDev, devs));
+
+     //calling NCCL communication API. Group API is required when using
+     //multiple devices per thread
+    NCCLCHECK(ncclGroupStart());
+    for (int i = 0; i < nDev; ++i)
+      NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i], size, ncclFloat, ncclSum,
+          comms[i], s[i]));
+    NCCLCHECK(ncclGroupEnd());
+
+    //synchronizing on CUDA streams to wait for completion of NCCL operation
+    for (int i = 0; i < nDev; ++i) {
+      CUDACHECK(cudaSetDevice(i));
+      CUDACHECK(cudaStreamSynchronize(s[i]));
+    }
+
+    //free device buffers
+    for (int i = 0; i < nDev; ++i) {
+      CUDACHECK(cudaSetDevice(i));
+      CUDACHECK(cudaFree(sendbuff[i]));
+      CUDACHECK(cudaFree(recvbuff[i]));
+    }
+
+    //finalizing NCCL
+    for(int i = 0; i < nDev; ++i)
+        ncclCommDestroy(comms[i]);
+
+    printf("Success \n");
+    return 0;
+}
+
+
+
+
+
+
